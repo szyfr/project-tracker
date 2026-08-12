ada12f657ab38d8c9757618f16ec0bf2017b1530
szyfr <neraj.mendoza@gmail.com>
2026-08-12 13:41:35 +0800

    feat: redirect home to login
    Route root through the login page instead of a static welcome page.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::redirect('/', 'login')->name('home');
 
 Route::resource('projects', ProjectController::class)->except(['create', 'edit']);
 
